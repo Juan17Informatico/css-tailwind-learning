92975c62b2dfe9fa4c59eac7c60156d8b81d3da5
JuanCM <juancampuzano2356@gmail.com>
2025-05-29 15:16:12 -0500

    feature: Add dark mode

--- curso-1/src/App.jsx
+++ curso-1/src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import { Chat } from './components/Chat.jsx';
 import { Notifcation } from './components/Notification/Notifcation.jsx';
 
 export const App = () => {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const handleDarkMode = () => {
+        setDarkMode(!darkMode);
+    }
+
     return (
-        <div className='bg-blue-300 flex justify-center items-center h-screen'>
+        <div className={`bg-gray-300 ${darkMode ? 'dark:bg-gray-800' : ''} transition-all flex justify-center items-center h-screen`}>
             {/* <Chat /> */}
             {/* <Notifcation /> */}
-            <button className='bg-gradient-to-t from-blue-900 to-blue-400 text-white px-4 py-2 rounded shadow-md focus:ring hover:bg-blue-500 transition-all hover:transform active:translate-y-1' >
+            <button  onClick={handleDarkMode} className='bg-gradient-to-t from-blue-900 to-blue-400 text-white px-4 py-2 rounded shadow-md focus:ring hover:bg-blue-500 transition-all hover:transform active:translate-y-1' >
                 Este es el mejor botón
             </button>
         </div>
